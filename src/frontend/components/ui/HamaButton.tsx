c0af076f5413bce7312cab12329bd81826dc618e
jongsik <kjsik11@gmail.com>
2022-03-16 14:24:47 +0900

    fix: remove target:_blank on hamabutton

--- src/frontend/components/ui/HamaButton.tsx
+++ src/frontend/components/ui/HamaButton.tsx
@@ -7,7 +7,6 @@ export default function HamaButton() {
   return (
     <NextLink href="https://hama.app/?referral=bFR9cqM4DHUkg46k">
       <a
-        target="_blank"
         className={cn(
           'border border-gray-300 py-2 px-10 inline-flex justify-center items-center font-bold rounded-8 rounded-lg hover:bg-black hover:text-white transition-colors',
         )}
